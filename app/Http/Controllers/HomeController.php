185843d66d10595854796771f6d4b4a160d8f15e
Shourav Saha <shourav939@gmail.com>
2023-03-19 01:51:41 +0600

    dynamically food menu showing user interface

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,19 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Food;
 
 class HomeController extends Controller
 {
     //
     public function index(){
-        return view('home');
+    $food_menu_data = Food::all();
+        return view('home', compact('food_menu_data'));
     }
     public function redirect(){
+        $food_menu_data = Food::all();
         $userType = Auth::user()->usertype;
         if($userType == 1){
             return view('admin.adminhome');
         }else{
-            return view('home');
+            return view('home', compact('food_menu_data'));
         }
     }
 }
